8cb80e9c248be91d706754639a68a5a3a89ba45f
xenxi <antoniom.diaz.moreno@gmail.com>
2022-12-06 19:09:29 +0100

    👔 add wrapping logic

---
+++ 01-Day/gifs-wrapper.ts
@@ -0,0 +1,6 @@
+export function wrapping(gifts: Array<String>) : Array<String> {
+  return gifts.map(gif => {
+    const giftWrap = '*'.repeat(gif.length + 2)
+    return `${giftWrap}\n*${gif}*\n${giftWrap}`
+  })
+}
---
+++ test/01-Day/gifts-wrapper.test.ts
@@ -0,0 +1,39 @@
+import { wrapping } from "../../01-Day/gifs-wrapper";
+
+
+test("Test #02", () => {
+  const gifts = ["cat", "game", "socks"];
+
+  const result = wrapping(gifts);
+
+  expect(result).toStrictEqual([
+    "*****\n*cat*\n*****",
+    "******\n*game*\n******",
+    "*******\n*socks*\n*******",
+  ]);
+});
+test("Test #03", () => {
+  const gifts = ["midu"];
+
+  const result = wrapping(gifts);
+
+  expect(result).toStrictEqual([
+    "******\n*midu*\n******"
+  ]);
+});
+test("Test #04", () => {
+  const gifts = ["a"];
+
+  const result = wrapping(gifts);
+
+  expect(result).toStrictEqual([
+    "***\n*a*\n***"
+  ]);
+});
+test("Test #05", () => {
+  const gifts = new Array<string>;
+
+  const result = wrapping(gifts);
+
+  expect(result).toStrictEqual([]);
+});
