2261758d5ab46905bcb2817e20cf8d671664df9e
xenxi <antoniom.diaz.moreno@gmail.com>
2022-12-06 19:52:33 +0100

    ✅ add impl

--- 04-Day/boxes-fitter.ts
+++ 04-Day/boxes-fitter.ts
@@ -1,5 +1,18 @@
-export function fitsInOneBox(boxes: Array<Box>): boolean {
-  return false;
+export function fitsInOneBox(boxes: Array<Box>) {
+  const biggerSide = (box: Box): number =>
+    Math.max(...(Object.values(box) as Array<number>));
+  const sortBoxes = (a: Box, b: Box = { l: 0, w: 0, h: 0 }): number =>
+    biggerSide(a) < biggerSide(b) ? -1 : 1;
+  const boxFit = (a: Box, b: Box): boolean =>
+    a.h < b.h && a.l < b.l && a.w < b.w;
+
+  return (
+    boxes.sort(sortBoxes).filter((box: Box, index: number) => {
+      const nextIndex = index + 1;
+      if (nextIndex >= boxes.length) return true;
+      return boxFit(box, boxes[nextIndex]);
+    }).length == boxes.length
+  );
 }
 interface Box {
   l: number;
