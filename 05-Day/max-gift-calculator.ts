8e86b6add1b041e822da2354cc04a1b20e4d5258
xenxi <antoniom.diaz.moreno@gmail.com>
2022-12-06 22:30:25 +0100

    ✅ add impl

--- 05-Day/max-gift-calculator.ts
+++ 05-Day/max-gift-calculator.ts
@@ -3,5 +3,14 @@ export function getMaxGifts(
   maxGifts: number,
   maxCities: number
 ): number {
-  return 0;
+  console.log(giftsCities.length, 1 << giftsCities.length);
+  return (
+    new Array(1 << giftsCities.length)
+      .fill(0)
+      .map((g, i) => giftsCities.filter((g2, j) => i & (1 << j)))
+      .filter((x) => x.length <= maxCities && x.length > 0)
+      .map((com) => com.reduce((a, b) => a + b))
+      .filter((i) => i <= maxGifts)
+      .sort((a, b) => b - a)[0] || 0
+  );
 }
